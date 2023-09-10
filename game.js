a2b193105fc2d391fe34dd6e104b0057ae4c8442
AndresFLK <andrescrptycol@gmail.com>
2023-09-10 14:56:41 -0600

    Selection Functions

--- game.js
+++ game.js
@@ -1,9 +1,41 @@
 var buttonColors = ["red","blue","yellow","green",];
 var gamePattern = [];
+var userPattern = [];
 
 function nextSequence(){
     return Math.floor(Math.random() * 4);  
 }
 
-var chosenColor = buttonColors[nextSequence];
-gamePattern.push(chosenColor);
+function play(){
+    randomNumber = nextSequence();
+    
+    var chosenColor = buttonColors[randomNumber];
+    switch(chosenColor){
+        case "red":
+            var audio = new Audio("./sounds/red.mp3");
+            audio.play();
+        case "blue":
+            var audio = new Audio("./sounds/blue.mp3");
+            audio.play();
+        case "yellow":
+            var audio = new Audio("./sounds/yellow.mp3");
+            audio.play();
+        case "green":
+            var audio = new Audio("./sounds/green.mp3");
+            audio.play();
+    }
+    $("#"+chosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    gamePattern.push(chosenColor); 
+}
+
+
+$(".btn").click(function(){
+    var userChose = $(this).attr("id");
+     userPattern.push(userChose);
+    console.log(userPattern);
+})
+    
+
+
+
+
